3fed50cd126f419971eec497caa0191cff285264
Jason Weinzierl <weinzierljason@gmail.com>
2020-12-24 22:25:21 -0600

    Expanded coverage of Querier to cover basic functionality.

--- BHS.DataAccess.Tests/Core/QuerierTests.cs
+++ BHS.DataAccess.Tests/Core/QuerierTests.cs
@@ -1,6 +1,7 @@
 ﻿using BHS.DataAccess.Tests;
 using Moq;
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
@@ -18,6 +19,20 @@ namespace BHS.DataAccess.Core.Tests
         public QuerierTests()
         {
             Subject = new Querier(MockData.CreateDbConnectionFactory().Object);
+        }
+
+        [Fact]
+        public async Task ExecuteScalarAsync_PreparesCommand()
+        {
+            MockData.ScalarCell = new object();
+            var mockFcn = new Mock<Action<IDbCommand>>();
+
+            _ = await Subject.ExecuteScalarAsync<object>("connstr", "Get", mockFcn.Object);
+
+            Assert.Equal("connstr", MockData.ConnectionStringName);
+            Assert.Equal("Get", MockData.CommandText);
+            Assert.Equal(CommandType.StoredProcedure, MockData.CommandType);
+            mockFcn.Verify(f => f.Invoke(It.IsAny<IDbCommand>()), Times.Once, "Expected configureCommand to be invoked");
         }
 
         [Theory]
@@ -38,14 +53,8 @@ namespace BHS.DataAccess.Core.Tests
         [Fact]
         public async Task ExecuteReaderAsync_PreparesCommand()
         {
-            var table = new DataTable();
-            table.Columns.Add("Col1");
-            table.Rows.Add(1);
-            table.Rows.Add(2);
-            table.Rows.Add(3);
-            MockData.ReaderResultset = table;
+            MockData.ReaderResultset = new DataTable();
             var mockFcn = new Mock<Action<IDbCommand>>();
-            mockFcn.Setup(f => f.Invoke(It.IsAny<IDbCommand>()));
 
             _ = await Subject.ExecuteReaderAsync("connstr", "Get", mockFcn.Object, (IDataReader dr, ref object model) => { });
 
@@ -56,19 +65,78 @@ namespace BHS.DataAccess.Core.Tests
         }
 
         [Fact]
-        public async Task ExecuteReaderAsync_ReadsAllData()
+        public async Task ExecuteReaderAsync_ReadsEachRow()
+        {
+            var table = new DataTable();
+            table.Columns.Add("Col1");
+            table.Rows.Add("a");
+            table.Rows.Add("b");
+            table.Rows.Add("c");
+            MockData.ReaderResultset = table;
+            var mockFiller = new Mock<FillDelegate<object>>();
+
+            _ = await Subject.ExecuteReaderAsync("connstr", "Get", null, mockFiller.Object);
+
+            mockFiller.Verify(f => f(It.IsAny<IDataReader>(), ref It.Ref<object>.IsAny), Times.Exactly(3), "Expected fillOutput to be called for each row.");
+        }
+
+        [Fact]
+        public async Task ExecuteReaderAsync_ReturnsEmptyObjectWhenNoRows()
+        {
+            MockData.ReaderResultset = new DataTable();
+
+            var result = await Subject.ExecuteReaderAsync<List<int>>("connstr", "Get", null, (IDataReader dr, ref List<int> models) => { });
+
+            Assert.NotNull(result);
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public async Task ExecuteReaderAsync_AsyncEnumerable_PreparesCommand()
+        {
+            MockData.ReaderResultset = new DataTable();
+            var mockFcn = new Mock<Action<IDbCommand>>();
+
+            _ = await Subject.ExecuteReaderAsync("connstr", "Get", mockFcn.Object, (IDataRecord dr) => new object()).ToListAsync();
+
+            Assert.Equal("connstr", MockData.ConnectionStringName);
+            Assert.Equal("Get", MockData.CommandText);
+            Assert.Equal(CommandType.StoredProcedure, MockData.CommandType);
+            mockFcn.Verify(f => f.Invoke(It.IsAny<IDbCommand>()), Times.Once, "Expected configureCommand to be invoked");
+        }
+
+        [Fact]
+        public async Task ExecuteReaderAsync_AsyncEnumerable_ReadsAllData()
         {
             var table = new DataTable();
             table.Columns.Add("Col1");
             table.Rows.Add("val1");
             table.Rows.Add(DBNull.Value);
+            table.Rows.Add("val2");
             MockData.ReaderResultset = table;
 
             var result = await Subject.ExecuteReaderAsync("connstr", "Get", null, dr => "Value").ToListAsync();
 
             Assert.NotNull(result);
-            Assert.Equal(2, result.Count);
-            //todo
+            Assert.Equal(3, result.Count);
+
+            Assert.Equal("Value", result[0]);
+            Assert.Equal("Value", result[1]);
+            Assert.Equal("Value", result[2]);
+        }
+
+        [Fact]
+        public async Task ExecuteNonQueryAsync_PreparesCommand()
+        {
+            MockData.NonQueryRowsAffected = 1;
+            var mockFcn = new Mock<Action<IDbCommand>>();
+
+            _ = await Subject.ExecuteNonQueryAsync("connstr", "Get", mockFcn.Object);
+
+            Assert.Equal("connstr", MockData.ConnectionStringName);
+            Assert.Equal("Get", MockData.CommandText);
+            Assert.Equal(CommandType.StoredProcedure, MockData.CommandType);
+            mockFcn.Verify(f => f.Invoke(It.IsAny<IDbCommand>()), Times.Once, "Expected configureCommand to be invoked");
         }
 
         [Fact]
@@ -92,23 +160,31 @@ namespace BHS.DataAccess.Core.Tests
         }
 
         [Fact]
-        public async Task ExecuteNonQueryAsync_OutputParameters()
+        public async Task ExecuteNonQueryAsync_Parameters_PreparesCommand()
         {
             MockData.NonQueryRowsAffected = 1;
+            var mockFcn = new Mock<Action<IDbCommand>>();
 
-            var result = await Subject.ExecuteNonQueryAsync("connstr", "Process", cmd =>
-            {
-                cmd.Parameters.Add(new MockDbDataParameter { ParameterName = "@para", Value = "value" });
-            }, c => "Value");
+            _ = await Subject.ExecuteNonQueryAsync("connstr", "Get", mockFcn.Object, cmd => "Value");
 
             Assert.Equal("connstr", MockData.ConnectionStringName);
-            Assert.Equal("Process", MockData.CommandText);
+            Assert.Equal("Get", MockData.CommandText);
+            Assert.Equal(CommandType.StoredProcedure, MockData.CommandType);
+            mockFcn.Verify(f => f.Invoke(It.IsAny<IDbCommand>()), Times.Once, "Expected configureCommand to be invoked");
+        }
+
+        [Fact]
+        public async Task ExecuteNonQueryAsync_Parameters_ReadsFromCommand()
+        {
+            MockData.NonQueryRowsAffected = 1;
+            var mockParamReader = new Mock<ParameterDelegate<object>>();
+            mockParamReader.Setup(r => r(It.IsAny<IDbCommand>()))
+                .Returns("Value");
+
+            var result = await Subject.ExecuteNonQueryAsync("connstr", "Process", null, mockParamReader.Object);
 
             Assert.Equal("Value", result);
-            Assert.Equal(CommandType.StoredProcedure, MockData.CommandType);
-            Assert.Equal("@para", MockData.Parameters[0].ParameterName);
-            Assert.Equal("value", MockData.Parameters[0].Value);
-            // todo: verify NonQuery was called
+            mockParamReader.Verify();
         }
     }
 }
