90f66a4c8679f1633f17bd54304e5288a1074819
Jason Weinzierl <weinzierljason@gmail.com>
2020-12-29 20:26:22 -0600

    Added unit tests on generalized db connection factory.

---
+++ BHS.DataAccess.Tests/Core/DbConnectionFactoryTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using Xunit;
+
+namespace BHS.DataAccess.Core.Tests
+{
+    public class DbConnectionFactoryTests
+    {
+        private readonly DbConnectionFactory Subject;
+
+        private string ConnectionStringName;
+
+        public DbConnectionFactoryTests()
+        {
+            const string connectionStringsSectionName = "ConnectionStrings";
+            var inMemoryData = new Dictionary<string, string>
+            {
+                { $"{connectionStringsSectionName}:db", "mock connection string" },
+                { $"{connectionStringsSectionName}:db_ProviderName", "MockDbProviderFactory" },
+                { $"{connectionStringsSectionName}:missingprovider", "mock connection string" },
+                { $"{connectionStringsSectionName}:badProviderName", "mock connection string" },
+                { $"{connectionStringsSectionName}:badProviderName_ProviderName", "BadDbProviderFactory" }
+            };
+
+            var inMemoryConfig = new ConfigurationBuilder()
+                .AddInMemoryCollection(inMemoryData)
+                .Build();
+
+            Subject = new DbConnectionFactory(inMemoryConfig, null);
+
+            var mockConnection = new Mock<DbConnection>();
+            mockConnection.SetupSet(c => c.ConnectionString = It.IsAny<string>())
+                .Callback<string>(connStrName => ConnectionStringName = connStrName);
+
+            var mockPrvdrFctry = new Mock<DbProviderFactory>();
+            mockPrvdrFctry.Setup(f => f.CreateConnection())
+                .Returns(mockConnection.Object);
+
+            DbProviderFactories.RegisterFactory("MockDbProviderFactory", mockPrvdrFctry.Object);
+        }
+
+        [Fact]
+        public void CreateConnection_Happy()
+        {
+            var result = Subject.CreateConnection("db");
+
+            Assert.NotNull(result);
+            Assert.Equal("mock connection string", ConnectionStringName);
+        }
+
+        [Fact]
+        public void CreateConnection_ThrowsOnMissingConnectionString()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                _ = Subject.CreateConnection("missingdb");
+            });
+        }
+
+        [Fact]
+        public void CreateConnection_ThrowsOnMissingProviderName()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                _ = Subject.CreateConnection("missingprovider");
+            });
+        }
+
+        [Fact]
+        public void CreateConnection_ThrowsOnProviderFactoryNotRegistered()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                _ = Subject.CreateConnection("badProviderName");
+            });
+        }
+    }
+}
