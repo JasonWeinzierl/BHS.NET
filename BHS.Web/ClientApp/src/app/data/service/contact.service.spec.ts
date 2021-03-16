7b36981850b9e82a62fc6f6f95ee7ac9c7d7c0ad
Jason Weinzierl <weinzierljason@gmail.com>
2021-03-15 21:50:04 -0500

    Replace get with inject in contact service test.

--- BHS.Web/ClientApp/src/app/data/service/contact.service.spec.ts
+++ BHS.Web/ClientApp/src/app/data/service/contact.service.spec.ts
@@ -3,10 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { ContactService } from './contact.service';
 
 describe('ContactService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+let service: ContactService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ContactService);
+  });
 
   it('should be created', () => {
-    const service: ContactService = TestBed.get(ContactService);
     expect(service).toBeTruthy();
   });
 });
