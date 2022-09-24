f9ae85501df38f145fbde42c23d6f8b74f999ad1
Jason Weinzierl <weinzierljason@gmail.com>
2022-09-24 08:54:38 -0500

    Simplify composition root tests

--- tests/BHS.Web.Tests/Controllers/ControllersTests.cs
+++ tests/BHS.Web.Tests/Controllers/ControllersTests.cs
@@ -1,107 +1,52 @@
-using BHS.Web.Controllers;
+﻿using BHS.Web.Controllers;
 using BHS.Web.IoC;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
-namespace BHS.Web.Tests.Controllers
+namespace BHS.Web.Tests.Controllers;
+
+[Trait("Category", "CompositionRootTest")]
+public class ControllersTests
 {
-    [Trait("Category", "CompositionRootTest")]
-    public class ControllersTests
+    private readonly IServiceCollection _services;
+
+    public ControllersTests()
     {
-        private readonly IServiceCollection _services;
+        var inMemoryConfig = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string>
+            {
+                { "SendGridClientOptions:ApiKey", "fake api key" }
+            })
+            .Build();
 
-        public ControllersTests()
-        {
-            var inMemoryConfig = new ConfigurationBuilder()
-                .AddInMemoryCollection(new Dictionary<string, string>
-                {
-                    { "SendGridClientOptions:ApiKey", "fake api key" }
-                })
-                .Build();
+        _services = new ServiceCollection()
+                .AddSingleton<IConfiguration>(inMemoryConfig)
+                .AddLogging()
+                .AddBhsDomain(inMemoryConfig)
+                .AddBhsInfrastructure();
+    }
 
-            _services = new ServiceCollection()
-                    .AddSingleton<IConfiguration>(inMemoryConfig)
-                    .AddLogging()
-                    .AddBhsDomain(inMemoryConfig)
-                    .AddBhsInfrastructure();
-        }
+    public static IEnumerable<object[]> Controllers => new[]
+    {
+        new[] { typeof(AuthorController) },
+        new[] { typeof(BannersController) },
+        new[] { typeof(BlogController) },
+        new[] { typeof(ContactUsController) },
+        new[] { typeof(ErrorController) },
+        new[] { typeof(LeadershipController) },
+        new[] { typeof(PhotosController) },
+    };
 
-        [Fact]
-        public void AuthorController_Resolves()
-        {
-            _services.AddTransient<AuthorController>();
-            using var serviceProvider = _services.BuildServiceProvider();
+    [Theory]
+    [MemberData(nameof(Controllers))]
+    public void Controller_Resolves(Type controllerType)
+    {
+        _services.AddTransient(controllerType);
+        using var serviceProvider = _services.BuildServiceProvider();
 
-            var controller = serviceProvider.GetService<AuthorController>();
+        var controller = serviceProvider.GetService(controllerType);
 
-            Assert.NotNull(controller);
-        }
-
-        [Fact]
-        public void BannersController_Resolves()
-        {
-            _services.AddTransient<BannersController>();
-            using var serviceProvider = _services.BuildServiceProvider();
-
-            var controller = serviceProvider.GetService<BannersController>();
-
-            Assert.NotNull(controller);
-        }
-
-        [Fact]
-        public void BlogController_Resolves()
-        {
-            _services.AddTransient<BlogController>();
-            using var serviceProvider = _services.BuildServiceProvider();
-
-            var controller = serviceProvider.GetService<BlogController>();
-
-            Assert.NotNull(controller);
-        }
-
-        [Fact]
-        public void ContactUsController_Resolves()
-        {
-            _services.AddTransient<ContactUsController>();
-            using var serviceProvider = _services.BuildServiceProvider();
-
-            var controller = serviceProvider.GetService<ContactUsController>();
-
-            Assert.NotNull(controller);
-        }
-
-        [Fact]
-        public void ErrorController_Resolves()
-        {
-            _services.AddTransient<ErrorController>();
-            using var serviceProvider = _services.BuildServiceProvider();
-
-            var controller = serviceProvider.GetService<ErrorController>();
-
-            Assert.NotNull(controller);
-        }
-
-        [Fact]
-        public void LeadershipController_Resolves()
-        {
-            _services.AddTransient<LeadershipController>();
-            using var serviceProvider = _services.BuildServiceProvider();
-
-            var controller = serviceProvider.GetService<LeadershipController>();
-
-            Assert.NotNull(controller);
-        }
-
-        [Fact]
-        public void PhotosController_Resolves()
-        {
-            _services.AddTransient<PhotosController>();
-            using var serviceProvider = _services.BuildServiceProvider();
-
-            var controller = serviceProvider.GetService<PhotosController>();
-
-            Assert.NotNull(controller);
-        }
+        Assert.NotNull(controller);
     }
 }
