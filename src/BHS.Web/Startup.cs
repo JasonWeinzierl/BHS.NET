7eebdd6c5c8e32230585c739271703f2acd3ce29
Jason Weinzierl <weinzierljason@gmail.com>
2023-01-09 20:32:32 -0600

    Removed now-unused Configuration from Startup.

--- src/BHS.Web/Startup.cs
+++ src/BHS.Web/Startup.cs
@@ -6,13 +6,6 @@ namespace BHS.Web;
 
 public class Startup
 {
-    public Startup(IConfiguration configuration)
-    {
-        Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllers();
