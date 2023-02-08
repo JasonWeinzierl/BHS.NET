4f7d2ae92e415fc824e34d3625d978c4901a800f
Jason Weinzierl <weinzierljason@gmail.com>
2023-02-07 22:15:24 -0600

    Switch post card dumb component to OnPush.

--- src/BHS.Web/ClientApp/src/app/modules/blog/components/post-card/post-card.component.ts
+++ src/BHS.Web/ClientApp/src/app/modules/blog/components/post-card/post-card.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { PostPreview } from '@data/blog';
 
 @Component({
   selector: 'app-post-card[post]',
   templateUrl: './post-card.component.html',
-  styleUrls: ['./post-card.component.scss']
+  styleUrls: ['./post-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PostCardComponent {
   @Input() post!: PostPreview;
