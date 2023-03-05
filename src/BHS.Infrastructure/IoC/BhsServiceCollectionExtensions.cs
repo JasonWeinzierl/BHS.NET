cec120ea6c4160b26aecce183560f3015230187f
Jason Weinzierl <weinzierljason@gmail.com>
2023-03-04 23:55:54 -0600

    Bump mongo driver.

--- src/BHS.Infrastructure/IoC/BhsServiceCollectionExtensions.cs
+++ src/BHS.Infrastructure/IoC/BhsServiceCollectionExtensions.cs
@@ -50,6 +50,7 @@ public static class BhsServiceCollectionExtensions
             {
                 builder.Subscribe<CommandStartedEvent>(e => logger.LogDebug("{CommandName} - {Command}", e.CommandName, e.Command.ToJson()));
             };
+            clientSettings.LinqProvider = MongoDB.Driver.Linq.LinqProvider.V2; // TODO: V3 causes errors; investigate and resolve.
             return new MongoClient(clientSettings);
         });
 
