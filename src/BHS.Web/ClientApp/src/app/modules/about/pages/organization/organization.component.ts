b24f9c18e0c6e7dea20a4f598dd8e1ae135e7225
Jason Weinzierl <weinzierljason@gmail.com>
2023-03-06 01:09:18 -0600

    Better loading indicator in officers.

--- src/BHS.Web/ClientApp/src/app/modules/about/pages/organization/organization.component.ts
+++ src/BHS.Web/ClientApp/src/app/modules/about/pages/organization/organization.component.ts
@@ -1,4 +1,4 @@
-import { catchError, combineLatest, map, Observable, of } from 'rxjs';
+import { catchError, combineLatest, map, Observable, of, startWith } from 'rxjs';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Director, LeadershipService, Officer } from '@data/leadership';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -10,20 +10,21 @@ import { HttpErrorResponse } from '@angular/common/http';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OrganizationComponent {
-  vm$: Observable<{ officers: Array<Officer>, directors: Array<Director>, error?: string }>;
+  vm$: Observable<{ officers: Array<Officer>, directors: Array<Director>, isLoading: boolean, error?: string }>;
 
   constructor(
     private leadershipService: LeadershipService,
   ) {
     this.vm$ = combineLatest([this.leadershipService.getOfficers(), this.leadershipService.getDirectors()])
       .pipe(
-        map(value => ({ officers: value[0], directors: value[1] }) ),
+        map(value => ({ officers: value[0], directors: value[1], isLoading: false }) ),
+        startWith({ officers: [], directors: [], isLoading: true }),
         catchError((error: unknown) => {
           let msg = 'An error occurred';
           if (error instanceof HttpErrorResponse) {
             msg = error.message;
           }
-          return of({ officers: [], directors: [], error: msg });
+          return of({ officers: [], directors: [], isLoading: false, error: msg });
         }),
       );
   }
