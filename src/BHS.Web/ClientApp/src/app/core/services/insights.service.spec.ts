effc45bef40a89edec5402fcdc28154a48c01844
Jason Weinzierl <weinzierljason@gmail.com>
2023-03-26 22:53:12 -0500

    Fixed null injector error from AppEnvironment.

--- src/BHS.Web/ClientApp/src/app/core/services/insights.service.spec.ts
+++ src/BHS.Web/ClientApp/src/app/core/services/insights.service.spec.ts
@@ -1,11 +1,21 @@
+import { AppEnvironment } from 'src/environments';
 import { InsightsService } from './insights.service';
+import { RouterTestingModule } from '@angular/router/testing';
 import { TestBed } from '@angular/core/testing';
 
 describe('InsightsService', () => {
   let service: InsightsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [
+        {
+          provide: AppEnvironment,
+          useValue: new AppEnvironment(),
+        },
+      ],
+    });
     service = TestBed.inject(InsightsService);
   });
 
