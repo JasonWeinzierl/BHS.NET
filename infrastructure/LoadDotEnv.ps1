3a12caed1cc8e1dfc4887f9727c418aeb9517f0d
Jason Weinzierl <weinzierljason@gmail.com>
2023-04-03 00:55:13 -0500

    dotenv script now accepts a name to load a 2nd file

--- infrastructure/LoadDotEnv.ps1
+++ infrastructure/LoadDotEnv.ps1
@@ -1,9 +1,29 @@
-$variables = Select-String -Path '.env' -Pattern '^\s*[^\s=#]+=[^\s]+$' -Raw
+$envType = $args[0]
+$verbose = $args.Contains("--verbose")
 
-foreach ($var in $variables) {
-    $keyVal = $var -split '=', 2
-    $key = $keyVal[0].Trim()
-    $val = $keyVal[1].Trim('"')
-    [Environment]::SetEnvironmentVariable($key, $val)
-    #"$key=$([Environment]::GetEnvironmentVariable(($key)))"
+$envFiles = @(".env")
+
+if ($envType) {
+    $envFile = ".env.$envType"
+    if (Test-Path $envFile) {
+        $envFiles += $envFile
+    } else {
+        throw "File '$envFile' not found."
+    }
+}
+
+foreach ($envFile in $envFiles) {
+
+    $variables = Select-String -Path $envFile -Pattern '^\s*[^\s=#]+=[^\s]+$' -Raw
+
+    foreach ($var in $variables) {
+        $keyVal = $var -split '=', 2
+        $key = $keyVal[0].Trim()
+        $val = $keyVal[1].Trim('"')
+        [Environment]::SetEnvironmentVariable($key, $val)
+        if ($verbose) {
+            "$key=$([Environment]::GetEnvironmentVariable(($key)))"
+        }
+    }
+    
 }
