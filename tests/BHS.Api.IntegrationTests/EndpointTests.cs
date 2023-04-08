417601a736203c0f2d0133cdc60a29f1aa3618a8
Jason Weinzierl <weinzierljason@gmail.com>
2023-04-07 21:52:22 -0500

    Added new integration tests project with isolated mongodb.

---
+++ tests/BHS.Api.IntegrationTests/EndpointTests.cs
@@ -0,0 +1,30 @@
+﻿using BHS.Web;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using Xunit;
+
+namespace BHS.Api.IntegrationTests;
+
+[Trait("Category", "Integration")]
+public class EndpointTests : IClassFixture<MongoDbWebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+    private readonly HttpClient _httpClient;
+
+    public EndpointTests(MongoDbWebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+        _httpClient = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task HealthCheck_Ok()
+    {
+        var response = await _httpClient.GetAsync("/api/healthcheck/status");
+
+        string content = await response.Content.ReadAsStringAsync();
+
+        Assert.Equal("Healthy", content);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
---
+++ tests/BHS.Api.IntegrationTests/MongoDbWebApplicationFactory.cs
@@ -0,0 +1,65 @@
+﻿using EphemeralMongo;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
+
+namespace BHS.Api.IntegrationTests;
+
+/// <remarks>
+/// Spins up an isolated MongoDB instance alongside the test server.
+/// </remarks>
+/// <inheritdoc />
+public sealed class MongoDbWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+    private const int PORT = 27018;
+    private const string DATABASE = "bhs";
+
+    private readonly IMongoRunner _mongoRunner;
+    private bool _disposedValue;
+
+    public MongoDbWebApplicationFactory()
+    {
+        _mongoRunner = MongoRunner.Run(new MongoRunnerOptions
+        {
+            MongoPort = PORT,
+            StandardOuputLogger = null,
+
+            KillMongoProcessesWhenCurrentProcessExits = true,
+        });
+
+        // Ensure the database exists.
+        var client = new MongoClient($"mongodb://localhost:{PORT}/{DATABASE}");
+        client.GetDatabase(DATABASE).CreateCollection("logs");
+    }
+
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureAppConfiguration(config =>
+        {
+            config.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                // Replace any occurrence of the MongoDB connection string.
+                { "ConnectionStrings:bhsMongo", $"mongodb://localhost:{PORT}/{DATABASE}" },
+                { "Serilog:WriteTo:0:Args:databaseUrl", $"mongodb://localhost:{PORT}/{DATABASE}" },
+            });
+        });
+
+        return base.CreateHost(builder);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (disposing)
+            {
+                _mongoRunner.Dispose();
+            }
+
+            _disposedValue = true;
+        }
+
+        base.Dispose(disposing);
+    }
+}
