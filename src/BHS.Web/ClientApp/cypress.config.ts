4156dc1baafda7dece86a11a803cce9bd1ca53ca
Jason Weinzierl <weinzierljason@gmail.com>
2023-04-08 16:58:17 -0500

    Removed cypress component testing (unused).

--- src/BHS.Web/ClientApp/cypress.config.ts
+++ src/BHS.Web/ClientApp/cypress.config.ts
@@ -4,15 +4,6 @@ export default defineConfig({
 
   e2e: {
     baseUrl: 'http://localhost:4200',
-    supportFile: false,
-  },
-
-  component: {
-    devServer: {
-      framework: 'angular',
-      bundler: 'webpack',
-    },
-    specPattern: '**/*.cy.ts',
   },
 
 });
