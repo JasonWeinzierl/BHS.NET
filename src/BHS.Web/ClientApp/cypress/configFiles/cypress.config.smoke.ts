137843314356995f90d471a051a1ae33afe95b21
Jason Weinzierl <weinzierljason@gmail.com>
2023-04-15 01:55:41 -0500

    Added auth check to smoke tests.

--- src/BHS.Web/ClientApp/cypress/configFiles/cypress.config.smoke.ts
+++ src/BHS.Web/ClientApp/cypress/configFiles/cypress.config.smoke.ts
@@ -4,8 +4,17 @@ export default defineConfig({
 
   e2e: {
     // This will be overridden first by --config baseUrl=http://example.com and then by CYPRESS_BASE_URL.
-    baseUrl: 'http://localhost:4200/',
+    baseUrl: 'https://localhost:5001/', // When running locally, requires the backend api and mongo database to be running.
     specPattern: 'cypress/smokeTests/**/*.cy.ts',
+    env: {
+      // These will be overridden by CYPRESS_{name} environment variables, where the {name} is converted to camelCase.
+      auth0Domain: 'dev-wz656qr7u8q11d84.us.auth0.com',
+      auth0ClientId: 'MEQJ4p2Y10CLMRebfZAEV2o2zxJVNFk7',
+      auth0Audience: 'https://beltonhistoricalsociety.org/api/swagger/index.html',
+      // These credentials do not work on any published app; this Auth0 tenant is for local development only.
+      auth0TestUsername: 'noreply@beltonhistoricalsociety.org',
+      auth0TestPassword: 'This password is fine to publish!',
+    },
   },
 
 });
--- src/BHS.Web/ClientApp/cypress/smokeTests/liveness.cy.ts
+++ src/BHS.Web/ClientApp/cypress/smokeTests/liveness.cy.ts
@@ -5,4 +5,16 @@ describe('Liveness', () => {
     cy.request('/api/healthcheck/status').its('body').should('equal', 'Healthy');
   });
 
+  it('should authenticate with Auth0', () => {
+    cy.visit('/');
+
+    cy.login();
+
+    cy.visit('/admin');
+
+    cy.contains('Log out').click();
+
+    cy.url().should('contain', Cypress.config().baseUrl);
+  });
+
 });
