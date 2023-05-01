2ab620ff0bb2e8ad726b8d42e65704be11ebdab4
Jason Weinzierl <weinzierljason@gmail.com>
2023-04-30 23:07:47 -0500

    Refactored album page with OnPush and tests.

--- src/BHS.Web/ClientApp/src/app/modules/photos/pages/album-page/album-page.component.spec.ts
+++ src/BHS.Web/ClientApp/src/app/modules/photos/pages/album-page/album-page.component.spec.ts
@@ -1,41 +1,68 @@
-import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { ActivatedRoute, convertToParamMap, RouterLink } from '@angular/router';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AlbumPageComponent } from './album-page.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AlbumPhotos } from '@data/photos';
+import { AlertComponent } from 'ngx-bootstrap/alert';
+import { By } from '@angular/platform-browser';
 import { of } from 'rxjs';
 import { PhotosService } from '@data/photos/services/photos.service';
 import { RouterTestingModule } from '@angular/router/testing';
+
+const createAlbum = (): AlbumPhotos => ({
+  slug: 'album-three',
+  name: 'Album Three',
+  blogPostSlug: '1-post',
+  photos: [{
+    id: 'photo-four',
+    legacyId: 1,
+    imagePath: '/assets/img/2017/oldcityhall.jpg',
+    datePosted: new Date(),
+  }, {
+    id: 'photo-five',
+    legacyId: 2,
+    imagePath: '/assets/img/2017/oldcityhall.jpg',
+    datePosted: new Date(),
+  }, {
+    id: 'photo-six',
+    legacyId: 3,
+    imagePath: '/assets/img/2017/oldcityhall.jpg',
+    datePosted: new Date(),
+  }],
+});
 
 describe('AlbumPageComponent', () => {
   let component: AlbumPageComponent;
   let fixture: ComponentFixture<AlbumPageComponent>;
 
   beforeEach(async () => {
+    const photosService = jasmine.createSpyObj<PhotosService>('photosService', {'getAlbum': of(createAlbum())});
+
     await TestBed.configureTestingModule({
       imports: [
-        HttpClientTestingModule,
         RouterTestingModule,
       ],
       declarations: [
+        AlertComponent,
         AlbumPageComponent,
       ],
       providers: [
-        PhotosService,
+        {
+          provide: PhotosService,
+          useValue: photosService,
+        },
         {
           provide: ActivatedRoute,
           useValue: {
             'paramMap': of(convertToParamMap({
-              slug: '3',
-              id: '4',
+              slug: 'album-three',
+              id: 'photo-four',
             })),
           },
         },
       ],
     })
     .compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(AlbumPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -43,5 +70,43 @@ describe('AlbumPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should finish loading', () => {
+    const element = fixture.nativeElement as HTMLElement;
+
+    expect(element.querySelector('.spinner-grow'))
+      .withContext('no loading')
+      .toBeNull();
+  });
+
+  it('should not show error', () => {
+    const element = fixture.nativeElement as HTMLElement;
+
+    expect(element.querySelector('.alert-danger')?.textContent)
+      .withContext('no error message')
+      .toBeUndefined();
+  });
+
+  it('should show album name', () => {
+    const element = fixture.nativeElement as HTMLElement;
+
+    expect(element.textContent)
+      .toContain('Album Three');
+  });
+
+  it('should render router links', () => {
+    const linkDebugElements = fixture.debugElement.queryAll(By.directive(RouterLink));
+    const routerLinks = linkDebugElements.map(de => de.injector.get(RouterLink));
+
+    expect(routerLinks.map(l => l.href)).toEqual([
+      '/apps/photos/album/album-three/photo/photo-six',
+      '/apps/photos/album/album-three',
+      '/apps/blog/entry/1-post',
+      '/apps/photos/album/album-three/photo/photo-five',
+      '/apps/photos/album/album-three/photo/photo-six',
+      '/apps/photos/album/album-three',
+      '/apps/photos/album/album-three/photo/photo-five',
+    ]);
   });
 });
--- src/BHS.Web/ClientApp/src/app/modules/photos/pages/album-page/album-page.component.ts
+++ src/BHS.Web/ClientApp/src/app/modules/photos/pages/album-page/album-page.component.ts
@@ -1,70 +1,69 @@
 import { ActivatedRoute, Router } from '@angular/router';
 import { AlbumPhotos, Photo, PhotosService } from '@data/photos';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { catchError, map, Observable, of, startWith, switchMap } from 'rxjs';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
+
+type AlbumPageVm = Observable<{ album?: AlbumPhotos, currentPhoto?: Photo, previousPhotoId: string, nextPhotoId: string, isLoading: boolean, error?: string }>;
 
 @Component({
   selector: 'app-album-page',
   templateUrl: './album-page.component.html',
   styleUrls: ['./album-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.Default, // TODO: Refactor to OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AlbumPageComponent implements OnInit {
-  isLoading = true;
-  album?: AlbumPhotos;
-  currentPhoto?: Photo;
-
-  previousPhotoId = '';
-  nextPhotoId = '';
-
-  error?: string;
+export class AlbumPageComponent {
+  vm$: AlbumPageVm;
 
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private photosService: PhotosService,
-  ) { }
-
-  ngOnInit(): void {
-    this.activatedRoute.paramMap.subscribe(params => {
-      const albumSlug = params.get('slug');
-      if (!albumSlug) {
-        this.error = 'Failed to get album slug from URL.';
-        return;
-      }
-
-      const photoId = params.get('id');
-      if (!photoId) {
-        this.error = 'Failed to get photo id from URL.';
-        return;
-      }
-
-      this.loadAlbum(albumSlug, photoId);
-    });
-  }
-
-  private loadAlbum(albumSlug: string, photoId: string): void {
-    this.photosService.getAlbum(albumSlug)
-      .subscribe({next: response => {
-        this.album = response;
-        this.currentPhoto = this.album.photos.find(p => p.id === photoId);
-
-        if (!this.currentPhoto) {
-          this.router.navigate(['not-found'], { replaceUrl: true });
-        } else {
-          const currentIndex = this.album.photos.indexOf(this.currentPhoto);
-
-          const previousIndex = currentIndex - 1 < 0 ? this.album.photos.length - 1 : currentIndex - 1;
-          const nextIndex = currentIndex + 1 >= this.album.photos.length ? 0 : currentIndex + 1;
-
-          this.previousPhotoId = this.album.photos[previousIndex].id;
-          this.nextPhotoId = this.album.photos[nextIndex].id;
+  ) {
+    this.vm$ = this.activatedRoute.paramMap.pipe(
+      map(params => {
+        const albumSlug = params.get('slug');
+        if (!albumSlug) {
+          throw new Error('Failed to get album slug from URL.');
         }
-      }, error: (error: unknown) => {
-        if (error instanceof HttpErrorResponse) {
-          this.error = error.message;
+
+        const photoId = params.get('id');
+        if (!photoId) {
+          throw new Error('Failed to get photo id from URL.');
         }
-      }})
-      .add(() => this.isLoading = false);
+
+        return { albumSlug, photoId };
+      }),
+      switchMap(({ albumSlug, photoId }) => this.photosService.getAlbum(albumSlug).pipe(
+        map(album => {
+          const currentIndex = album.photos.findIndex(p => p.id === photoId);
+
+          if (currentIndex < 0) {
+            this.router.navigate(['not-found'], { replaceUrl: true })
+              .catch((err: unknown) => console.error(err));
+            return { previousPhotoId: '', nextPhotoId: '', error: 'Not found.', isLoading: false };
+          } else {
+            const currentPhoto = album.photos[currentIndex];
+
+            const previousIndex = currentIndex - 1 < 0 ? album.photos.length - 1 : currentIndex - 1;
+            const nextIndex = currentIndex + 1 >= album.photos.length ? 0 : currentIndex + 1;
+
+            const previousPhotoId = album.photos[previousIndex].id;
+            const nextPhotoId = album.photos[nextIndex].id;
+
+            return { album, currentPhoto, previousPhotoId, nextPhotoId, isLoading: false };
+          }
+        }),
+      )),
+      startWith({ previousPhotoId: '', nextPhotoId: '', isLoading: true }),
+      catchError((err: unknown) => {
+        let msg = 'An error occurred';
+        if (err instanceof HttpErrorResponse) {
+          msg = err.message;
+        }
+        console.error(msg, err);
+        return of({ previousPhotoId: '', nextPhotoId: '', error: msg, isLoading: false });
+      }),
+    );
   }
 }
