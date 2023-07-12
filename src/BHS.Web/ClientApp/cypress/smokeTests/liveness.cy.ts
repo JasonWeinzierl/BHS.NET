11413360a0c48e2e83084f36f133f42aa4f849a2
Jason Weinzierl <weinzierljason@gmail.com>
2023-07-11 23:27:51 -0500

    test: fix flaky smoke test when SendGrid is degraded

--- src/BHS.Web/ClientApp/cypress/smokeTests/liveness.cy.ts
+++ src/BHS.Web/ClientApp/cypress/smokeTests/liveness.cy.ts
@@ -2,7 +2,14 @@
 describe('Liveness', () => {
 
   it('should pass the health check', () => {
-    cy.request('/api/healthcheck/status').its('body').should('equal', 'Healthy');
+    cy.request('/api/healthcheck/status').then(response => {
+      expect(response.status).to.equal(200);
+      expect(response.body).to.be.oneOf(['Healthy', 'Degraded']);
+
+      if (response.body === 'Degraded') {
+        cy.log('WARNING: The health check reports Degraded.');
+      }
+    });
   });
 
   it('should authenticate with Auth0', () => {
