95fd690cb49dc0ee018ec1933f226485c681c1c4
Jason Weinzierl <weinzierljason@gmail.com>
2023-07-24 00:34:09 -0500

    feat: view access token on admin page

--- src/BHS.Web/ClientApp/src/app/modules/admin/pages/admin-index/admin-index.component.ts
+++ src/BHS.Web/ClientApp/src/app/modules/admin/pages/admin-index/admin-index.component.ts
@@ -1,6 +1,7 @@
 import { AuthService, User } from '@auth0/auth0-angular';
+import { catchError, map, Observable, of } from 'rxjs';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import jwt_decode, { JwtPayload } from 'jwt-decode';
 
 @Component({
   selector: 'app-admin-index',
@@ -10,11 +11,25 @@ import { Observable } from 'rxjs';
 })
 export class AdminIndexComponent {
   user$: Observable<User | null | undefined>;
+  accessToken$: Observable<JwtPayload | { message: string }>;
 
   constructor(
     private readonly auth: AuthService,
   ) {
     this.user$ = this.auth.user$;
+    this.accessToken$ = this.auth.getAccessTokenSilently({ cacheMode: 'cache-only' }).pipe(
+      map(token => jwt_decode<JwtPayload>(token)),
+      catchError((err: unknown) => {
+        let message = 'An error occurred. ';
+        if (typeof err === 'string') {
+          message += err;
+        } else if (typeof err === 'object' && err && 'message' in err && typeof err.message === 'string') {
+          message += err.message;
+        }
+        console.error(message, err);
+        return of({ message });
+      }),
+    );
   }
 
   handleLogout(): void {
