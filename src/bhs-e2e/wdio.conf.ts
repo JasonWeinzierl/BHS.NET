f017ced119847bfdae153800efd0c6db2ab4d10b
Jason Weinzierl <weinzierljason@gmail.com>
2024-11-06 12:47:57 -0600

    chore: run new wdio e2e tests as smoke tests

--- src/bhs-e2e/wdio.conf.ts
+++ src/bhs-e2e/wdio.conf.ts
@@ -1,6 +1,15 @@
 import * as core from '@actions/core';
 import registerCustomCommands from './test/wdio-commands/register-custom-commands';
 
+const defaultRunnerEnv: Record<string, string> = {
+  E2E_auth0Domain: 'dev-wz656qr7u8q11d84.us.auth0.com',
+  E2E_auth0ClientId: 'MEQJ4p2Y10CLMRebfZAEV2o2zxJVNFk7',
+  E2E_auth0Audience: 'https://beltonhistoricalsociety.org/api/swagger/index.html',
+  // These credentials do not work on any published app; this Auth0 tenant is for local development only.
+  E2E_auth0TestUsername: 'noreply@beltonhistoricalsociety.org',
+  E2E_auth0TestPassword: 'This password is fine to publish!',
+};
+
 export const config: WebdriverIO.Config = {
   specs: [
     './test/specs/**/*.ts',
@@ -8,15 +17,6 @@ export const config: WebdriverIO.Config = {
 
   // This will be overridden by wdio --baseUrl=http://example.com
   baseUrl: 'https://localhost:5001', // When running locally, requires the backend api and mongo database to be running.
-  runnerEnv: {
-    // These will be overridden by environment variables.
-    E2E_auth0Domain: 'dev-wz656qr7u8q11d84.us.auth0.com',
-    E2E_auth0ClientId: 'MEQJ4p2Y10CLMRebfZAEV2o2zxJVNFk7',
-    E2E_auth0Audience: 'https://beltonhistoricalsociety.org/api/swagger/index.html',
-    // These credentials do not work on any published app; this Auth0 tenant is for local development only.
-    E2E_auth0TestUsername: 'noreply@beltonhistoricalsociety.org',
-    E2E_auth0TestPassword: 'This password is fine to publish!',
-  },
 
   maxInstances: 10,
   capabilities: [{
@@ -29,11 +29,19 @@ export const config: WebdriverIO.Config = {
     browserName: 'edge',
   }],
 
-  onPrepare(_cfg, capabilities) {
+  onPrepare(cfg, capabilities) {
     if (!core.platform.isMacOS && Array.isArray(capabilities)) {
       core.warning(`Safari is not available on ${core.platform.platform}. Removing Safari capabilities.`);
       const safariIndex = capabilities.findIndex(cap => 'browserName' in cap && cap.browserName === 'safari');
       capabilities.splice(safariIndex, 1);
+    }
+
+    cfg.runnerEnv = {};
+    for (const key in defaultRunnerEnv) {
+      // eslint-disable-next-line n/no-process-env
+      if (process.env[key] === undefined) {
+        cfg.runnerEnv[key] = defaultRunnerEnv[key];
+      }
     }
   },
 
