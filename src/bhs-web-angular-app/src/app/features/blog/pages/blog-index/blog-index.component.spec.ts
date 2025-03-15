012444a1f76cbc9b5979f7ef7a365553bf91bdba
Jason Weinzierl <weinzierljason@gmail.com>
2025-03-15 10:07:17 -0500

    feat: add New Post button to blog homepage

--- src/bhs-web-angular-app/src/app/features/blog/pages/blog-index/blog-index.component.spec.ts
+++ src/bhs-web-angular-app/src/app/features/blog/pages/blog-index/blog-index.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
 import { MockComponent, MockProvider } from 'ng-mocks';
 import { EMPTY } from 'rxjs';
 import { BlogIndexComponent } from './blog-index.component';
@@ -18,8 +20,12 @@ describe('BlogIndexComponent', () => {
         MockComponent(PostsSearchComponent),
       ],
       providers: [
+        provideRouter([]),
         MockProvider(BlogService, {
           getCategories: () => EMPTY,
+        }),
+        MockProvider(AuthService, {
+          isAuthenticated$: EMPTY,
         }),
       ],
     })
--- src/bhs-web-angular-app/src/app/features/blog/pages/blog-index/blog-index.component.ts
+++ src/bhs-web-angular-app/src/app/features/blog/pages/blog-index/blog-index.component.ts
@@ -1,6 +1,8 @@
 import { AsyncPipe } from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
 import { catchError, map, of, startWith } from 'rxjs';
 import { CategoriesListViewComponent } from '../../components/categories-list-view/categories-list-view.component';
 import { PostsSearchComponent } from '../../components/posts-search/posts-search.component';
@@ -15,10 +17,12 @@ import { BlogService } from '@data/blog';
     PostsSearchComponent,
     CategoriesListViewComponent,
     AsyncPipe,
+    RouterLink,
   ],
 })
 export class BlogIndexComponent {
   private readonly blogService = inject(BlogService);
+  private readonly auth = inject(AuthService);
 
   categoriesVm$ = this.blogService.getCategories().pipe(
     map(categories => ({ categories, isLoading: false, error: null })),
@@ -33,4 +37,6 @@ export class BlogIndexComponent {
       return of({ categories: [], isLoading: false, error: msg });
     }),
   );
+
+  isAuthenticated$ = this.auth.isAuthenticated$;
 }
