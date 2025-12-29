381cbacbc6b6b29b48814707b9c001618fa56561
Jason Weinzierl <weinzierljason@gmail.com>
2025-12-29 12:55:57 -0600

    test(e2e): check if already logged in

--- src/bhs-e2e/test/robots/login.robot.ts
+++ src/bhs-e2e/test/robots/login.robot.ts
@@ -1,8 +1,14 @@
+import * as core from '@actions/core';
 import getRunnerEnv from '../get-runner-env';
 
 class LoginRobot {
   async loginFromEnvironmentVariables() {
     const runnerEnv = getRunnerEnv();
+
+    if (await this.isUserLoggedIn(runnerEnv.E2E_auth0ClientId)) {
+      core.notice('User is already logged in; skipping.');
+      return;
+    }
 
     const baseUrl = browser.options.baseUrl;
     if (!baseUrl) {
@@ -24,10 +30,14 @@ class LoginRobot {
     }, { timeoutMsg: 'Timed out waiting for redirect after Auth0 login.' });
 
     await browser.waitUntil(async () => {
-      return (await browser.execute(clientId => {
-        return localStorage.getItem(`@@auth0spajs@@::${clientId}::@@user@@`);
-      }, runnerEnv.E2E_auth0ClientId)) !== null;
+      return this.isUserLoggedIn(runnerEnv.E2E_auth0ClientId);
     }, { timeoutMsg: 'Timed out waiting for Auth0 to persist to storage.' });
+  }
+
+  private async isUserLoggedIn(auth0ClientId: string): Promise<boolean> {
+    return (await browser.execute(clientId => {
+      return localStorage.getItem(`@@auth0spajs@@::${clientId}::@@user@@`);
+    }, auth0ClientId)) !== null;
   }
 }
 export default new LoginRobot();
